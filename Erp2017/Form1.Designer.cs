08179163b6636949f587e36161a78093aec62003
Ludo <Ludo@ludobruynseels.com>
2017-11-28 21:37:54 +0100

    add data project

--- Erp2017/Form1.Designer.cs
+++ Erp2017/Form1.Designer.cs
@@ -28,12 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dgCustomers = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgCustomers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgCustomers
+            // 
+            this.dgCustomers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgCustomers.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgCustomers.Location = new System.Drawing.Point(0, 0);
+            this.dgCustomers.Name = "dgCustomers";
+            this.dgCustomers.Size = new System.Drawing.Size(922, 261);
+            this.dgCustomers.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(922, 261);
+            this.Controls.Add(this.dgCustomers);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgCustomers)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgCustomers;
     }
 }
 
--- Erp2017/Form1.cs
+++ Erp2017/Form1.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Erp2017.Data.InvoicesDataSetTableAdapters;
 
 namespace Erp2017
 {
@@ -15,6 +9,16 @@ namespace Erp2017
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var ds = new Data.InvoicesDataSet();
+            var ta = new CustomersTableAdapter();
+
+            ta.Fill(ds.Customers);
+
+            dgCustomers.DataSource = ds.Customers;
         }
     }
 }
