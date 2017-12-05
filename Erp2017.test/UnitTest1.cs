0623766bce145a72cd5396eb9083e7323c345f33
Ludo <Ludo@ludobruynseels.com>
2017-12-05 23:06:16 +0100

    disable test

--- Erp2017.test/UnitTest1.cs
+++ Erp2017.test/UnitTest1.cs
@@ -1,14 +1,13 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿//using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace Erp2017.test
-{
-    [TestClass]
-    public class UnitTest1
-    {
-        [TestMethod]
-        public void TestMethod1()
-        {
-        }
-    }
-}
+//namespace Erp2017.test
+//{
+//    [TestClass]
+//    public class UnitTest1
+//    {
+//        [TestMethod]
+//        public void TestMethod1()
+//        {
+//        }
+//    }
+//}
