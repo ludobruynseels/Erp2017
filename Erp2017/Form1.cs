7a5d6e7dc2028cf9bea45f68516a445264bae448
betelgeuze <ludo.bruynseels@gmail.com>
2020-04-26 22:09:52 +0200

    merge 2 folders

--- Erp2017/Form1.cs
+++ Erp2017/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Erp2017.Data.InvoicesDataSetTableAdapters;
 
 namespace Erp2017
 {
@@ -15,6 +16,16 @@ namespace Erp2017
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var ds = new Data.InvoicesDataSet();
+            var ta = new CustomersTableAdapter();
+
+            ta.Fill(ds.Customers);
+
+            dgCustomers.DataSource = ds.Customers;
         }
     }
 }
